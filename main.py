a4c0d36e0f83d4996779f2c9a5bfd2ab08d98c5b
Fenek-code <65533264+Fenek-code@users.noreply.github.com>
2021-01-03 01:21:57 +0300

    add pause and countdown
    Added a pause, now you do not need to constantly restart the program. Now you can track how long the program has "worked". And also removed, so hated by everyone, global variables.

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
+import datetime
 import time
-import pyautogui as auto
+import pyautogui
 import os
 import keyboard
 
-amount = 0
 
-
-def run(_time, stop):
-    global amount
-
+def run(_time, stop, start, amount, status, start_time):
     for i in range(_time):
+
+        # Set time to zero (only needed for display)
+        temp = datetime.timedelta(seconds=0)
+        if status:
+            # Find out how much time has passed since launch
+            temp = str(datetime.timedelta(seconds=int(time.time() - start_time)))
+            # Counting
+            amount += 1
+        else:
+            i = 0  # don't let the loop move until we start the process
+
         time.sleep(1)
-        os.system('CLS')
-        print(f"time: {_time - i} |\t for stop pinch:'{stop}'\t |\tamount:{amount}")
+        os.system('CLS')  # Screen cleaner, only works on Windows
+
+        print(f"{'(work)' if status else '[resting]'}"
+              f"\n\nleft to click: {_time - (i)}"
+              f"\nfor stop  pinch: '{stop}'"
+              f"\nfor start pinch: '{start}'"
+              f"\namount: {amount}"
+              f"\ntime at work: {temp}"
+              )
 
         if keyboard.is_pressed(stop):
-            return
+            status = False  # click through time - off
 
-    auto.click(button='right')
-    amount += 1
-    run(_time, stop)
+        if keyboard.is_pressed(start):
+            start_time = time.time()  # write the start time
+
+            amount = 0  # cancel the account
+            status = True  # click through time - on
+
+    if status:
+        pyautogui.click(button='right')  # press the right mouse button
+    run(_time, stop, start, amount, status, start_time)  # do recursion
 
 
 def main():
     run(
-        int(input("Время между кликами в секундах: \t")),
-        input("Клавиша останова: \t")
+        int(input("Time between clicks in seconds: \t")),
+        input("Stop key: \t"),
+        input("Start key: \t"),
+        0,
+        False,
+        time.time()
     )
     return
 
